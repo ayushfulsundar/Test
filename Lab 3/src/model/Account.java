e3dc108a59dc011e7ff869d9549c6233e3240925
ayushfulsundar <fulsundar.a@northeastern.edu>
2024-09-30 19:15:05 -0400

    Committed

---
+++ Lab 3/src/model/Account.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ayush
+ */
+public class Account {
+    
+    private String bankname;
+    private String routingnumber;
+    private String accountnumber;
+    private int balance;
+
+    public String getBankname() {
+        return bankname;
+    }
+
+    public void setBankname(String bankname) {
+        this.bankname = bankname;
+    }
+
+    public String getRoutingnumber() {
+        return routingnumber;
+    }
+
+    public void setRoutingnumber(String routingnumber) {
+        this.routingnumber = routingnumber;
+    }
+
+    public String getAccountnumber() {
+        return accountnumber;
+    }
+
+    public void setAccountnumber(String accountnumber) {
+        this.accountnumber = accountnumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    
+    @Override
+    public String toString(){
+        return bankname;
+    }
+    
+}
